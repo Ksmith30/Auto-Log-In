b97b8d4a1670ba708d96fc928283a08a24e40f3f
Ksmith30 <kylesmith0630@gmail.com>
2018-04-06 09:34:43 -0500

    Split into functions

--- work_script.py
+++ work_script.py
@@ -1,21 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
+browser = webdriver.Firefox()
+link = 'https://admin.uark.edu/natcgi/uwologon'
+logon_button = '/html/body/div/form[1]/fieldset/table/tbody/tr[2]/td/input'
+hourly_time_button = '/html/body/form/table[1]/tbody/tr[5]/td[1]/a'
+time_clock_button = '/html/body/form/table[1]/tbody/tr[2]/td[1]/a'
+validate_button = '/html/body/form/div[2]/input[2]'
+save_button = '/html/body/form/div[2]/input[3]'
+
+
+def get_browser_type():
+    browser_name = get_default_browser_name()
+
+
+def type_into_browser_element_by_id(identification, text):
+    element = browser.find_element_by_id(identification)
+    element.send(keys)
+
+
+def click_browser_element_by_xpath(xpath):
+    element = browser.find_element_by_xpath(xpath)
+    element.click()
+
+
 user = raw_input("What is your username?")
 pwd = raw_input("What is your password?")
-driver = webdriver.Firefox()
-driver.get("https://admin.uark.edu/natcgi/uwologon")
-elem = driver.find_element_by_id("LDAP_UARK_ID")
-elem.send_keys(user)
-elem = driver.find_element_by_id("LDAP_PASSWORD")
-elem.send_keys(pwd)
-elem = driver.find_element_by_name("logonSB")
-elem.click()
-elem = driver.find_element_by_link_text('Hourly Time')
-elem.click()
-elem = driver.find_element_by_link_text('Time Clock')
-elem.click()
-elem = driver.find_element_by_xpath('/html/body/form/div[2]/input[2]')
-elem.click()
-elem = driver.find_element_by_xpath('/html/body/form/div[2]/input[3]')
-elem.click()
+browser.get(link)
+type_into_browser_element_by_id("LDAP_UARK_ID", user)
+type_into_browser_element_by_id("LDAP_PASSWORD", pwd)
+click_browser_element_by_xpath(logon_button)
+click_browser_element_by_xpath(hourly_time_button)
+click_browser_element_by_xpath(time_clock_button)
+click_browser_element_by_xpath(validate_button)
+click_browser_element_by_xpath(save_button)
